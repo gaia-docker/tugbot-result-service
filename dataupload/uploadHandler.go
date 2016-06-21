ed981d737549583f009e32e45c4330f4c1b64879
kornfeld <lital.kornfeld@hpe.com>
2016-06-21 14:50:27 +0300

    added mainfile query param, added line brakes in html

--- dataupload/uploadHandler.go
+++ dataupload/uploadHandler.go
@@ -3,10 +3,13 @@ package dataupload
 import (
 	"compress/gzip"
 	"errors"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/gaia-docker/tugbot-result-service/pool"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
 	"strings"
 )
 
@@ -29,16 +32,35 @@ func (uh UploadHandler) ServeHTTP(writer http.ResponseWriter, request *http.Requ
 
 	retStatus := http.StatusOK
 	body, err := getBody(request)
+	params := request.URL.Query()
+	mainFileName := params.Get("mainfile")
 	if err != nil {
 		retStatus = http.StatusBadRequest
 		log.Error("Error fetching request body. ", err)
 	} else {
-		result, err := uh.uploader.Upload(body)
+		resultDir, err := uh.uploader.Upload(body)
 		if err == nil {
-			uh.hub.Broadcast(result)
+			uh.broadcast(*resultDir, mainFileName)
 		}
 	}
 	writer.WriteHeader(retStatus)
+}
+
+func (uh UploadHandler) broadcast(resultDir, mainFileName string) {
+
+	var message string = ""
+	if mainFileName != "" {
+		content, err := ioutil.ReadFile(filepath.Join(resultDir, mainFileName))
+		if err != nil {
+			log.Errorf("error reading file content <%s> %s", mainFileName, err)
+		} else {
+			message = string(content)
+		}
+	}
+	if message == "" {
+		message = fmt.Sprintf("Received test results, no main file was specified <result dir=%s>", resultDir)
+	}
+	uh.hub.Broadcast(&message)
 }
 
 func getBody(request *http.Request) (io.ReadCloser, error) {
